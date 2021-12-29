d2a83fb1cfc11adfa9f7f05997ce8c4a3e03bceb
Tristan <seekstris>
2021-12-29 19:29:29 +0500

    Install WordPress Plugin in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    'gatsby-source-wordpress', {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // Specify the URL of the WordPress source
+        baseUrl: `trisec.io`,
+        protocol: `https`,
+        // Indicates if a site is hosted on WordPress.com
+        hostingWPCOM: false,
+        // Specify which URL structures to fetch
+        includedRoutes: [
+          '**/posts',
+          '**/tags',
+          '**/categories'
+        ]
+      }}
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -31,6 +46,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
